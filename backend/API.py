a7d74a21a1d729b57d7257600ca80c0656aad6ea
nosinska <natalia.osinska@nokia.com>
2025-05-27 12:49:36 +0200

    db upgrade

--- backend/API.py
+++ backend/API.py
@@ -1,1 +1,23 @@
 from fastapi import FastAPI
+from db.DB import DB
+
+app = FastAPI()
+db = DB("db_connection")
+
+@app.get("/statistics")
+def get_statistics():
+    """Get statistics from the database.
+
+    Returns:
+        A dictionary with the statistics.
+    """
+    data = db.get_all_users()
+    return data
+
+
+if __name__ == "__main__":
+    # data = db.get_all_users()
+    # print(data)
+
+    # import uvicorn
+    # uvicorn.run(app)
--- db/DB.py
+++ db/DB.py
@@ -1,9 +1,12 @@
 import mysql.connector
 import setup.config as config
+from datetime import datetime, timedelta
+import jwt
 
 class DB:
     def __init__(self, section_name:str):
         data = config.get_from_config("config.ini", section_name)
+        secret = config.get_from_config("config.ini", "jwt")
         self.my_db = mysql.connector.connect(
             host=data["host"],
             port=3306,
@@ -13,12 +16,14 @@ class DB:
             autocommit=True
         )
 
-    def get_user_data(self, user_id:int) -> dict:
-        cursor = self.my_db.cursor(dictionary=True)
-        cursor.execute("SELECT * FROM emails WHERE user_id = %s", (user_id,))
-        result = cursor.fetchall()
-        cursor.close()
-        return result
+    def get_user_data(self, user_id:int, token:str) -> dict:
+        if self.verify_token(user_id, token):
+            cursor = self.my_db.cursor(dictionary=True)
+            cursor.execute("SELECT * FROM users WHERE id = %s", (user_id,))
+            result = cursor.fetchone()
+            cursor.close()
+            return result
+        return{ "error": "Invalid token or user ID." }
 
     def get_all_users(self):
         cursor = self.my_db.cursor(dictionary=True)
@@ -26,3 +31,20 @@ class DB:
         result = cursor.fetchall()
         cursor.close()
         return result
+    
+    def login(self, user_id:str, hash:str) -> bool:
+        cursor = self.my_db.cursor()
+        cursor.execute("SELECT * FROM users WHERE id = %s AND password = %s", (user_id, hash))
+        result = cursor.fetchone()
+        token = None
+        if result is not None:
+            token = { "id": user_id, "end_time": f"{datetime.now() + timedelta(hours=1)}" }
+            token = jwt.encode(token, key=self.secret["secret"], algorithm="HS256")
+        cursor.close()
+        return token
+    
+    def verify_token(self, user_id:str, token:str) -> bool:
+        decoded = jwt.decode(token, key=self.secret["secret"], algorithms=["HS256"])
+        if user_id == decoded["user_id"] and datetime(decoded["end_time"]) > datetime.now():
+            return True
+        return False
